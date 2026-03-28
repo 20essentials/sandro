aeb9ee45bf52d4a44b77bac40d215b9dca35ff23
DominicDev <s0l0estudi4r@gmail.com>
2026-03-28 14:04:11 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'SANDRO';
+export const nameSongs = [
+	"ROSA... ROSA", /*1*/
+	"PENUMBRAS", /*2*/
+	"PENAS", /*3*/
+	"TRIGAL", /*4*/
+	"TE PROPONGO", /*5*/
+	"PORQUE YO TE AMO", /*6*/
+	"SE TE NOTA", /*7*/
+	"LA VIDA SIGUE IGUAL", /*8*/
+	"UNA MUCHACHA Y UNA GUITARRA", /*9*/
+	"COMO LO HICE YO", /*10*/
+	"EL MANIQUÍ", /*11*/
+	"AVE DE PASO", /*12*/
+	"FÁCIL DE OLVIDAR", /*13*/
+	"PARÍS ANTE TI", /*14*/
+	"ES EL AMANTE", /*15*/
+	"PALABRAS VIEJAS", /*16*/
+	"AL ABRIR LA PUERTA", /*17*/
+	"TE PROPONGO", /*18*/
+	"TENGO", /*19*/
+	"GUITARRAS AL VIENTO", /*20*/
+]
